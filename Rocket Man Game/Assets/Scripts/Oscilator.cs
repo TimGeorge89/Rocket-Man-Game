8ffa6f1027b4be1a99c0570614f6532d9fd71cad
Tim <Fallenpyr@aol.com>
2022-04-24 21:51:28 -0400

    Adding Obstacle Movement
    Started Oscillation code

---
+++ Rocket Man Game/Assets/Scripts/Oscilator.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscilator : MonoBehaviour
+{
+    //Variables
+
+    Vector3 startingPos;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] [Range(0,1)] float movementFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPos = transform.position;
+        Debug.Log(startingPos);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+    }
+}
